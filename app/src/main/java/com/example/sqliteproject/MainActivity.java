e59e30c50c460a713a1c94b64c4c894eb82a6afd
IntepeZafer <intepezafer@gmail.com>
2022-07-14 17:21:34 +0300

    Bu projemde sqlite kullanımı ile ilgili bir çalışma projesi geliştirdim

---
+++ app/src/main/java/com/example/sqliteproject/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sqliteproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Musucians" , MODE_PRIVATE , null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS musucians (name VARCHAR , age INT)");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
